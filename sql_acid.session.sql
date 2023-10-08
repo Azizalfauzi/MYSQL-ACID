3e37a4c540938ac44f7411833318acaee4230d1a
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-10-09 05:06:08 +0700

    MYSQL ACID - Atomicity

--- sql_acid.session.sql
+++ sql_acid.session.sql
@@ -1,5 +1,9 @@
+--- ATOMICITY SUCCESS
+---
+DROP TABLE accounts;
+---
 CREATE TABLE accounts(
-    id VARCHAR(100),
+    id VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
     balance BIGINT NOT NULL,
     PRIMARY KEY (id)
@@ -9,3 +13,28 @@ DESC accounts;
 --- 
 ALTER TABLE accounts
 MODIFY id VARCHAR(100) NOT NULL;
+---
+START TRANSACTION;
+---
+INSERT INTO accounts(id, name, balance)
+VALUES('aziz', 'Aziz Alfa', 1000000);
+---
+SELECT *
+FROM accounts;
+---
+INSERT INTO accounts(id, name, balance)
+VALUES('betta', 'Betta Adi', 1500000);
+---
+COMMIT;
+--- ATOMICITY FAILED
+START TRANSACTION;
+---
+SELECT * FROM accounts;
+---
+DELETE FROM accounts
+WHERE id = 'aziz';
+--- 
+DELETE FROM accounts
+WHERE id = 'betta';
+---
+ROLLBACK;
